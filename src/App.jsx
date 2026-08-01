d8403e519c4db895c6875818151f845eef2ef126
pawel-orzel <p.orzel@redemptor.pl>
2026-08-01 18:50:33 +0200

    licznik

--- src/App.jsx
+++ src/App.jsx
@@ -479,7 +479,7 @@ export default function App() {
     </ErrorBoundary>
   );
 
-  const isPassportScreen = !user || (user && !userData && !showAdminForm && view !== 'admin') || (user && !user.isAnonymous && !userData && !showAdminForm && view !== 'admin');
+  const isPassportScreen = !user || (user && !userData && !showAdminForm && view !== 'admin');
   
   if (isPassportScreen) {
     return (
