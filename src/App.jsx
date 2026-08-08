d5ff4f88666670677d76f8aa16f22ce465096b76
pawel-orzel <p.orzel@redemptor.pl>
2026-08-09 01:05:42 +0200

    sss app

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ import {
 
 import FinalStage from './final';
 import { showAlert, showConfirm } from './modal';
-import ParticipantBatchPanel from './ParticipantBatchPanel';
+import ParticipantBatchPanel from './ParticipantBatchPanel.jsx';
 import RegRodo from './reg.RODO';
 
 const OWNER_UID = "fIGFNjIUm6Onldwe27qb7R9vvB63"; // WAŻNE: Wklej tutaj swoje UID z panelu Firebase Authentication
